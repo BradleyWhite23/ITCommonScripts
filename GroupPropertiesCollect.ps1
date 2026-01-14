b1612720ad5ac4389012f81e7dbed955e19a0e6e
Bradley <143959842+BradleyWhite23@users.noreply.github.com>
2026-01-14 09:08:49 +0000

    Add files via upload
    AD - Obtain Group user properties

---
+++ GroupPropertiesCollect.ps1
@@ -0,0 +1,14 @@
+﻿
+$properties = @(
+    'Name',
+    'UserPrincipalName',
+    'SamAccountName',
+    'Description'
+)
+
+$searchBase = 'OU=Year1,OU=Students,DC=your,DC=domain,DC=local' # Adjust this to your target OU
+
+Get-ADUser -Filter * -SearchBase $searchBase -Properties $properties |
+    Select-Object -Property $properties |
+    Export-Csv -Path 'C:\temp\ExportedList.csv' -NoTypeInformation -Encoding UTF8
+# REPLACE 'C:\temp\ExportedList.csv' with your desired output path
