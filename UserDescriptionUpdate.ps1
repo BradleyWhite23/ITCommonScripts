c6cbca5bcd6a8b6c82abf22447f4967ecd07bc69
Bradley <143959842+BradleyWhite23@users.noreply.github.com>
2026-01-14 09:19:59 +0000

    Add files via upload
    AD - Script to Update User Properties

---
+++ UserDescriptionUpdate.ps1
@@ -0,0 +1,15 @@
+﻿Import-Module ActiveDirectory
+
+$OuDn = "OU=Year1,OU=Students,DC=your,DC=domain,DC=local"  # change this
+$NewDescription = "Y1" #change this
+$Users = Get-ADUser -SearchBase $OuDn -Filter * -Properties Description
+
+foreach ($u in $Users) {
+    try {
+        Set-ADUser -Identity $u.DistinguishedName -Description $NewDescription -ErrorAction Stop
+        Write-Host "Updated $($u.SamAccountName) -> '$NewDescription'" -ForegroundColor Green
+    }
+    catch {
+        Write-Host "Failed $($u.SamAccountName): $($_.Exception.Message)" -ForegroundColor Red
+    }
+}
